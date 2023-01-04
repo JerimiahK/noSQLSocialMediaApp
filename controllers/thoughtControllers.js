bf22999a5fdc2428925f39ce8a2da6a83a976f37
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-04 15:36:14 -0700

    added thoughts controller

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,3 +1,54 @@
-//get all thoughts
-//get one thought
-//
+const { Thoughts, Users, Reactions } = require("../models");
+
+module.exports = {
+  getThoughts(req, res) {
+    Thoughts.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => {
+        return res.status(500).json(err);
+      });
+  },
+  getSingleThought(req, res) {
+    Thoughts.findOne({ _id: req.params.id })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createThought(req, res) {
+    Thoughts.create(req.body)
+      .then((thought) => {
+        return Users.findOneAndUpdate(
+          { _id: req.body.id },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "Tag created, but found no post with that ID" })
+          : res.json("Created the tag 🎉")
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  deleteThought(req, res) {
+    Thoughts.findOneAndDelete({ _id: req.params.id })
+      //   .then((user) =>
+      //     !user
+      //       ? res.status(404).json({ message: "No user with that ID" })
+      //       : Thoughts.deleteMany({ _id: { $in: user.thoughts } })
+      //   )
+      .then(() => res.json({ message: "user and students deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,28 +1,36 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reactions");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    maxlength: 280,
-    minlength: 1,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date().toLocaleDateString("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      }),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: new Date().toLocaleDateString("en-us", {
-      weekday: "long",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    }),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions;
